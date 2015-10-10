94bce4060d64873bcdf8a57feddcba3d29a9503b
Celdir <michael.earl7769@gmail.com>
2015-10-10 04:31:57 -0500

    Semi-complete backend for User, Clan, Team, and Game; and a gamemode called "Assassins".

---
+++ game/assassins.py
@@ -0,0 +1,41 @@
+from reactive import User, Team, Game, Clan
+from random import shuffle
+
+class Assassins(Game):
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
+        self.users = []
+        self.teams = []
+
+    def add_user(self, user):
+        user.alive = True
+        self.users.append(user)
+
+    def start_game(self):
+        shuffle(self.users)
+        for counter, user in enumerate(self.users):
+            if counter == len(self.users)-1:
+                self.users[counter].target = self.users[0]
+            else:
+                self.users[counter].target = self.users[counter+1]
+
+    def win(self, id):
+        for user in self.users:
+            if user.id == id:
+                user.add_score(1)
+                return
+
+    def kill(self, id):
+        for counter, user in enumerate(self.users):
+            if user.id == id:
+                user.alive = False
+                if counter == len(self.users)-1:
+                    self.users[counter-1].target = self.users[0]
+                else: 
+                    self.users[counter-1].target = self.users[counter+1]
+                user.leave_game()
+
+        if len(self.users) == 1:
+            self.game_over()
+
